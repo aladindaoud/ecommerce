412fdd4ee9c29bab89c2214f1ba54f2ec00cb086
aladindaoud <83234713+aladindaoud@users.noreply.github.com>
2024-03-18 00:54:37 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBa1U4Mldj4G4nP1w2mhMozFF1LQb9p6RE",
+  authDomain: "ecommerce-a3505.firebaseapp.com",
+  projectId: "ecommerce-a3505",
+  storageBucket: "ecommerce-a3505.appspot.com",
+  messagingSenderId: "223896136431",
+  appId: "1:223896136431:web:500cd06ba886516987091a"
 };
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
